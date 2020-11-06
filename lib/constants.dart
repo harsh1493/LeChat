c0de7007eb2588e8d03f8469e9f735582b5824bb
Harsh <harshrivastava1493@gmail.com>
2020-11-06 15:30:23 +0530

    First commit LE CHAT

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const sha1Key = "08:F4:B8:FF:D6:4B:47:6D:27:20:8E:41:83:0D:BD:E9:45:AF:83:58";
+const sha1Key = "08:F4:B8:FF:D6:4B:47:6D:27:20:8E:41:83:0D:BD:E9:45:AF:83:58ee";
 const sha256 =
     "EC:74:9C:F0:73:4E:29:18:9C:46:48:FD:65:17:EC:52:9A:FF:A0:DB:B4:0A:C7:82:5F:43:49:94:78:6D:11:F6";
 const kSendButtonTextStyle = TextStyle(
